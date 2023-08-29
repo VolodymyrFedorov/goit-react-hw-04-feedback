f742cc7f29171ec8537870653100fe0fd7c04353
VolodymyrFedorov <133118295+VolodymyrFedorov@users.noreply.github.com>
2023-08-29 13:27:59 +0300

    done?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,76 @@
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { AiFillLike } from 'react-icons/ai';
+import { FaHandPeace } from 'react-icons/fa';
+import { AiFillDislike } from 'react-icons/ai';
+import css from './App.module.css';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const iconsButton = {
+    good: <AiFillLike size="30px" className={css.icon_good} />,
+    neutral: <FaHandPeace size="30px" />,
+    bad: <AiFillDislike size="30px" className={css.icon_bad} />,
+  };
+
+  const onBtnClicked = option => {
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
+    const positivePercentage = Math.round((good / total) * 100);
+    return total ? positivePercentage : 0;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+  const options = ['good', 'neutral', 'bad'];
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrapper}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={onBtnClicked}
+          iconsButton={iconsButton}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
 };
